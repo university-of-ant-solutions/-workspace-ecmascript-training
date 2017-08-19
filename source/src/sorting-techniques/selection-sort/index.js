f51eeb873552ddc007c42b1163eb34010036c7c3
hoang <particle4dev@lehoangs-MacBook-Pro.local>
2017-08-19 21:48:59 +0700

    selection sort

---
+++ source/src/sorting-techniques/selection-sort/index.js
@@ -0,0 +1,19 @@
+function selection (arr) {
+  let i, j, min_idx, temp;
+  const length = arr.length;
+  for (i = 0; i < length-1; i++) {
+    min_idx = i;
+    for (j = i+1; j < length; j++) {
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
+      }
+    }
+    temp = arr[min_idx];
+    arr[min_idx] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
+}
+
+export default selection;
