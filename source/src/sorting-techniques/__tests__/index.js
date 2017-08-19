2c7cc8ee1b5b6b4d041d8c7148ef9affe3922db6
hoang <particle4dev@lehoangs-MacBook-Pro.local>
2017-08-19 21:46:45 +0700

    refractory test

---
+++ source/src/sorting-techniques/__tests__/index.js
@@ -0,0 +1,25 @@
+import test from 'tape';
+import bubble from '../bubble-sort/index.js';
+import insertion from '../insertion-sort/index.js';
+import selection from '../selection-sort/index.js';
+
+const algorithm = {
+  'bubble': bubble,
+  'insertion': insertion,
+  'selection': selection,
+};
+test('sorting techniques', (assert) => {
+  for (let prop in algorithm) {
+    if (algorithm.hasOwnProperty(prop)) {
+      test(`${prop}-sort \ return an ordered array`, (t) => {
+        t.plan(2);
+        let input = [4, 1, 3, 5, 2];
+        t.deepEqual(algorithm[prop](input), [1, 2, 3, 4, 5]);
+
+        input = [-4, 1, 3, -5, 2];
+        t.deepEqual(algorithm[prop](input), [-5, -4, 1, 2, 3]);
+      });
+    }
+  }
+  assert.end();
+});
